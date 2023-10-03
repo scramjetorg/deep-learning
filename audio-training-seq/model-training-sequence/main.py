8bd5614e5dd2a9b5a4abe907da2644c7c5beabaa
rnawfal <ray.nawfal@gmail.com>
2023-10-03 07:36:02 +0000

    add Sequential model function

--- audio-training-seq/model-training-sequence/main.py
+++ audio-training-seq/model-training-sequence/main.py
@@ -5,9 +5,92 @@ import tarfile
 import asyncio
 from scramjet import streams
 
-print("[INFO:] Connecting to cloud")
+import tensorflow as tf
+import tensorflow_io as tfio
+
+from keras import layers
+from keras import models
+from tensorflow.keras.layers import Conv2D, Dense, Flatten, MaxPooling2D
+from tensorflow.keras.losses import SparseCategoricalCrossentropy
+from tensorflow.keras.optimizers import Adam
+
+
+# Sequential model 
+def create_model(input_shape, norm_layer):
+    model = models.Sequential([
+        layers.Input(shape=input_shape),
+        # Downsample the input.
+        layers.Resizing(32, 32),
+        # Normalize.
+        norm_layer,
+        layers.Conv2D(32, 3, activation='relu'),
+        layers.Conv2D(64, 3, activation='relu'),
+        layers.MaxPooling2D(),
+        layers.Dropout(0.25),
+        layers.Flatten(),
+        layers.Dense(128, activation='relu'),
+        layers.Dropout(0.5),
+        layers.Dense(64, activation='relu'),
+        layers.Dense(32, activation='relu'),
+        layers.Dense(10, activation='softmax'),
+    ])
+    return model
+
+# Load checkpoint if available
+def load_checkpoint(model, checkpoint_dir):
+    checkpoint = tf.train.Checkpoint(model=model)
+    latest_checkpoint = tf.train.latest_checkpoint(checkpoint_dir)
+    if latest_checkpoint:
+        checkpoint.restore(latest_checkpoint)
+        print(f"Checkpoint '{latest_checkpoint}' restored!")
+
+def load_wav_16k_mono(filename):
+    # Load encoded wav file
+    file_contents = tf.io.read_file(filename)
+    # Decode wav (tensors by channels) 
+    wav, sample_rate = tf.audio.decode_wav(file_contents, desired_channels=1)
+    # Removes trailing axis
+    wav = tf.squeeze(wav, axis=-1)
+    sample_rate = tf.cast(sample_rate, dtype=tf.int64)
+    # Goes from 44100Hz to 16000hz - amplitude of the audio signal
+    wav = tfio.audio.resample(wav, rate_in=sample_rate, rate_out=16000)
+
+def preprocess(file_path): 
+    # Load files into 16kHz mono
+    wav = load_wav_16k_mono(file_path)
+    # Only read the first 3 secs
+    wav = wav[:16000]
+    # If file < 3s add zeros
+    zero_padding = tf.zeros([16000] - tf.shape(wav), dtype=tf.float32)
+    wav = tf.concat([zero_padding, wav],0)
+    # Use Short-time Fourier Transform
+    spectrogram = tf.signal.stft(wav, frame_length=255, frame_step=128)
+    # Convert to absolut values (no negatives)
+    spectrogram = tf.abs(spectrogram)
+    # Add channel dimension (needed by CNN later)
+    spectrogram = tf.expand_dims(spectrogram, axis=2)
+    return spectrogram 
+
+label_names = []
+def get_record(path):
+    path_dataset = os.listdir(path)
+    for folder in path_dataset:
+        label = folder
+        print(label.decode("utf-8"))
+        label = label.decode("utf-8")
+        if label not in label_names:
+           label_names.append(label)
+        else:
+           pass
+        path_folder = os.path.join(path, folder)
+        for file in os.listdir(path_folder):
+            dir_index = path_dataset.index(folder)
+            audio_dir = os.path.join(path_folder, file)
+            spectrogram = preprocess(audio_dir) 
+            yield spectrogram, dir_index
 
 def checkpoint_search(aws_key, aws_secret, bucket):
+    print("[INFO:] Connecting to cloud")
     session = boto3.session.Session()
     s3_client = session.client(
         service_name='s3',
@@ -81,6 +164,8 @@ def uncompress_tarfile(source_dir, desired_dir):
 
 async def run(context, input, key, secret, bucket, object):
 
+    dataset_path = "/PATH/TO/FOLDER/data/mini_speech_commands"
+
     key = key
     secret = secret
     bucket = bucket
@@ -100,6 +185,42 @@ async def run(context, input, key, secret, bucket, object):
     source_dir = file_path
     desired_dir = "temp_unzip"
     uncompress_tarfile(source_dir, desired_dir)
+
+    # Dataset generator    
+    dataset = tf.data.Dataset.from_generator(
+        get_record,
+        args=[dataset_path],
+        output_signature=(
+            tf.TensorSpec(shape=(124, 129, 1), dtype=tf.float32),
+            tf.TensorSpec(shape=(), dtype=tf.int32)))
+    
+    # Buffer the dataset
+    dataset = dataset.cache()
+    dataset = dataset.shuffle(buffer_size=12600)
+    dataset = dataset.batch(32)
+    dataset = dataset.prefetch(tf.data.AUTOTUNE) 
+
+    train = dataset.take(160)
+    test = dataset.skip(160).take(30)
+
+    print(f"Loading checkpoint to the Model...")
+    # Model checkpoint load
+    checkpoint_dir = "temp_unzip/cp.ckpt" 
+    input_shape= (124, 129, 1)
+    norm_layer = layers.Normalization()
+    # with `Normalization.adapt`.
+    norm_layer.adapt(data=train.map(map_func=lambda spec, label: spec))
+    model = create_model(input_shape, norm_layer)
+    load_checkpoint(model, checkpoint_dir)
+    model.summary()
+
+    model.compile(
+        optimizer=tf.keras.optimizers.Adam(), 
+        loss=tf.keras.losses.SparseCategoricalCrossentropy(), 
+        metrics=['accuracy'],
+    )
+
+    history = model.fit(train, validation_steps=8, epochs=20, validation_data=test)
 
     # Zip the checkpoint folder
     source_dir = "temp_unzip"
